612b04c82b5cb4f740751de05638260a16bc3db4
John Yenter-Briars <42982326+JohnYB1963@users.noreply.github.com>
2019-02-26 02:31:43 -0600

    First commit of codebase

---
+++ CustomerAppMVC/Controllers/CustomerController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace CustomerAppMVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        MFDDataClassDataContext dc = new MFDDataClassDataContext();
+
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+
+
+
+            return View();
+        }
+
+        public ActionResult QueryCustomers()
+        {
+            IEnumerable<Customer> customers = (from customer in dc.Customers
+                                               select customer).ToList();
+            IEnumerable<CustomerAppMVC.Models.Customer> mycustomers = customers.Select(cust => new Models.Customer
+            {
+                CustomerName = cust.CustomerName,
+                CustomerID = cust.CustomerID,
+                Location = cust.Location
+            }).ToList();
+
+            return View(mycustomers);
+        }
+
+        //NOTE: in order for ModelState.IsValid to trigger properly, the input type of the post action result MUST be the model itself
+        //duh I know
+        [HttpPost]
+        public ActionResult EditCustomer(CustomerAppMVC.Models.Customer model)
+        {
+            var currCust = (from cust in dc.Customers
+                            where cust.CustomerID == model.CustomerID
+                            select cust).FirstOrDefault();
+
+            if (ModelState.IsValid)
+            {
+                var result = (from Customer in dc.Customers
+                              where Customer.CustomerID == model.CustomerID
+                              select Customer).FirstOrDefault();
+
+                
+
+                result.CustomerName = model.CustomerName;
+                result.Location = model.Location;
+
+                dc.SubmitChanges();
+                return View((CustomerAppMVC.Models.Customer) result);
+
+            }
+            return View();
+        }
+
+
+        public ActionResult EditCustomer(int id)
+        {
+            CustomerAppMVC.Models.Customer myCust = (CustomerAppMVC.Models.Customer) (from customer in dc.Customers
+                                              where customer.CustomerID == id
+                                              select customer).FirstOrDefault();
+            return View(myCust);
+        }
+    }
+}
---
+++ CustomerAppMVC/Models/Customer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CustomerAppMVC.Models
+{
+    public class Customer
+    {
+        private string _CustomerName;
+        private string _Location;
+
+        [Required]
+        public int CustomerID { get; set; }
+        [Required]
+        public string CustomerName
+        {
+            get
+            {
+                if (this._CustomerName == null)
+                    return null;
+                else
+                    return this._CustomerName.Trim(); }
+            set { this._CustomerName = value; }
+            
+        }
+        [Required]
+        public string Location
+        {
+            get
+            {
+                if (this._Location == null)
+                    return null;
+                else
+                    return this._Location.Trim(); }
+            set{ this._Location = value; }
+        }
+
+        public static explicit operator Customer(CustomerAppMVC.Customer other)
+        {
+            return new Customer { CustomerID = other.CustomerID, CustomerName = other.CustomerName, Location = other.Location };
+        }
+
+    }
+}
